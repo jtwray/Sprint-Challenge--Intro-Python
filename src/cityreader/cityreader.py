ad69fc5da61c16fcb48f4d1f4fad71ab64bb1eb0
tucker <tuckerwray84@gmail.com>
2020-07-17 23:27:05 -0400

    using https://www.geeksforgeeks.org/working-csv-files-python/ to print the rows of csv file to a new array attempting to **spread in the values of each row to the bse Class City. I think i actually need to call for row[0] to get city name row[3] and row[4] to get lat and long as positional arguments to my Class instances

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,13 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+  def __init__(self,name,lat,lon):
+    self.name=name
+    self.lat = lat
+    self.lon = lon
 
+    #city,state_name,county_name,lat,lng,population,density,timezone,zips
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -16,15 +22,28 @@
 # should not be loaded into a City object.
 cities = []
 
-def cityreader(cities=[]):
+def cityreader(filename,cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  import csv
+  fields=[]
+  rows=[]
+    with open(str(filename), 'r') as csvfile:
+      csvreader= csv.reader(csvfile)
+      fields= next(csvreader)
+      for row in csvreader:
+        cities.append(City(**fields))
+        rows.append(row)
+
+
+
     
     return cities
 
-cityreader(cities)
+
+cityreader(Sprint-Challenge--Intro-Python\src\cityreader\cities.csv,cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
