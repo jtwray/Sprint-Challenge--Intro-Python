4d274ba2b145cfcc9452f82458b5f5dad3b8c305
tucker <tuckerwray84@gmail.com>
2020-07-17 21:50:12 -0400

    drives motocycles and Groundvehicles in oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -6,6 +6,7 @@
 class GroundVehicle():
     def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+
     def drive(self):
         return f'vroooom'
 
@@ -20,6 +21,15 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+        self.num_wheels=num_wheels
+        
+    
+    def drive(self):
+        return f'BRAAAAAP!!'
+
 
 vehicles = [
     GroundVehicle(),
@@ -29,6 +39,13 @@ vehicles = [
     Motorcycle(),
 ]
 
+
+
+
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+vroom=[print(i.drive()) for i in vehicles]
+
+
